6c37215e72dfa9caae951ab55df2ff5eab7eb291
dnyaneshwar giri <dnyaneshwargiri510@gmail.com>
2023-04-11 00:21:57 +0530

    added angular material widgets examples

---
+++ angular material widgets/src/app/material-table/material-table.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { MaterialTableComponent } from './material-table.component';
+
+describe('MaterialTableComponent', () => {
+  let component: MaterialTableComponent;
+  let fixture: ComponentFixture<MaterialTableComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ MaterialTableComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MaterialTableComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ angular material widgets/src/core/truncate.pipe.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { TruncatePipe } from './truncate.pipe';
+
+describe('Pipe: Truncatee', () => {
+  it('create an instance', () => {
+    let pipe = new TruncatePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
